8b2cf82e15d96399649249c96602b739c6ca4263
Raflores10 <120347604+Raflores10@users.noreply.github.com>
2023-03-10 15:17:50 -0800

    updating code

--- script.js
+++ script.js
@@ -26,6 +26,26 @@ function setCurrentWewather(Data){
         temp.textContent = "Temp: " + data.list[0].main.temp + " F°";
         wind.textContent = "Wind: " + data.list[0].wind.speed + " Mph";
         humidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+
+        for (let i = 0; i < data.list.length; i++) {
+            const element = data.list[i];
+            console.log(element);
+            if (element.dt_txt.includes("12:00:00")) {
+                var date = document.createElement("h2")
+                date.textContent = element.dt_txt;
+
+                var temp2 = document.createElement("p")
+                temp2.textContent = "Temp: " + element.main.temp + " F°";
+
+                var wind2 = document.createElement("p")
+                wind2="Wind: " + element.wind.speed + " Mph";
+
+                var hum = document.createElement("p")
+                hum.textContent = "Humidity: " + element.main.humidity + "%";
+
+                document.getElementById("weatherCards").append(date,temp2,wind2,hum)
+            }
+        }
     });
 } 
 
